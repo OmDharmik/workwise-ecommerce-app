4e081ef312796b072bd7bce6e9ea9fcfc844c763
om <omdharmik24@gmail.com>
2024-08-23 19:49:31 +0530

    feat: added add product seller

---
+++ frontend/app/seller/add-product/page.jsx
@@ -0,0 +1,117 @@
+'use client';
+
+import SellerNavbar from '@/app/components/SellerNavbar';
+import { useState } from 'react';
+
+const AddProduct = () => {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(null);
+
+  const handlerAddProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+
+    // Convert numeric fields to numbers
+    data.price = parseFloat(data.price);
+    data.discount = parseFloat(data.discount);
+
+    console.log(data);
+    setLoading(true);
+    setSuccess(null);
+
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/product/create`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result = await response.json();
+      if (response.ok) {
+        setSuccess('Product added successfully!');
+        console.log(result);
+      } else {
+        console.error('Error:', result);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <SellerNavbar />
+      <h1 className="flex justify-center font-bold text-4xl p-3">
+        Add Product
+      </h1>
+      <div className="flex flex-col">
+        <div className="h-1/2 bg-slate-200 mx-5 rounded-md mt-5">
+          <h2 className="font-semibold text-xl relative m-5">
+            Product Information
+          </h2>
+          <form onSubmit={handlerAddProduct}>
+            <div className="flex ">
+              <div className="flex flex-col gap-5 mt-10 w-3/5 p-5">
+                <input
+                  name="name"
+                  type="text"
+                  className="p-3 rounded-md"
+                  placeholder="Name"
+                  required
+                />
+                <input
+                  name="category"
+                  type="text"
+                  className="p-3 rounded-md"
+                  placeholder="Category"
+                  required
+                />
+                <input
+                  name="price"
+                  type="number"
+                  className="p-3 rounded-md"
+                  placeholder="Price"
+                  required
+                />
+                <input
+                  name="description"
+                  type="text"
+                  className="p-3 rounded-md"
+                  placeholder="Description"
+                />
+                <input
+                  name="discount"
+                  type="number"
+                  className="p-3 rounded-md"
+                  placeholder="Discount"
+                  required
+                />
+              </div>
+              <div className="flex flex-col justify-center items-center w-2/5">
+                <button
+                  type="submit"
+                  className="bg-blue-400 p-3 rounded-md transition ease-in-out hover:bg-green-600 delay-150 hover:scale-110 duration-300"
+                >
+                  {loading ? 'Adding...' : 'Add Product'}
+                </button>
+                {success && <p className="text-green-500 mt-2">{success}</p>}
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddProduct;
--- frontend/app/seller/products/page.jsx
+++ frontend/app/seller/products/page.jsx
@@ -4,16 +4,16 @@ import SellerLeftSideBar from '@/app/components/SellerLeftSideBar';
 import SellerNavbar from '@/app/components/SellerNavbar';
 import axios from 'axios';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const Products = () => {
   const pathname = usePathname();
+  const router = useRouter();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-
     const fetchProducts = async () => {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/product`,
@@ -38,23 +38,42 @@ const Products = () => {
         <SellerLeftSideBar pathname={pathname} className="flex flex-2" />
         <div className="h-screen w-full">
           <div className="container mx-auto p-4">
+            <div className="flex justify-end">
+              <button
+                onClick={() => {
+                  router.push('/seller/add-product');
+                }}
+                className="transition ease-in-out p-3 rounded-md delay-150 bg-green-300 hover:scale-110 hover:bg-green-500 duration-300 mr-10 hover:shadow-md"
+              >
+                Add Product
+              </button>
+            </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
               {products.map((product) => {
                 return (
-                  <div className="flex flex-col justify-center">
-                    <div
-                      key={product.id}
-                      className="bg-slate-200 flex flex-col items-center box-border h-60 w-60 p-4 border-4 rounded-md"
-                    >
-                      <div className="box-border h-48 w-48">
+                  <div className="flex justify-center mt-5" key={product.id}>
+                    <div className="bg-slate-200 flex flex-col items-center box-border h-60 w-60 p-4 border-4 rounded-md">
+                      <div className="box-border h-48 w-48 relative">
                         <Image
                           src={'/product-image.jpeg'}
-                          alt="product"
+                          alt={product.name}
                           layout="fill"
-                          className="object-contain"
+                          objectFit="contain"
+                          className="rounded-md"
                         />
                       </div>
-                      <div>{product.name}</div>
+                      <div className="mt-2 text-center">{product.name}</div>
+                      <div className="flex gap-5 p-2 mt-3">
+                        <button
+                          onClick={() => router.push('/seller/edit')}
+                          className="bg-blue-400 p-2 rounded-md"
+                        >
+                          Edit
+                        </button>
+                        <button className=" bg-red-400 p-2 rounded-md">
+                          Delete
+                        </button>
+                      </div>
                     </div>
                   </div>
                 );
